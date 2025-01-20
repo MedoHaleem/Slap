34a3030b060cedd5f25fe3fcb70c45b351bf2847
Medo Haleem <medo.a.haleem@gmail.com>
2025-01-20 20:39:58 +0200

    Add ChatRoomLive

--- lib/slap_web/live/chat_room_live.ex
+++ lib/slap_web/live/chat_room_live.ex
@@ -1,9 +1,28 @@
 defmodule SlapWeb.ChatRoomLive do
+  alias Slap.Chat.Room
+  alias Slap.Repo
   use SlapWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    room = Room |> Repo.all() |> List.first()
+    {:ok, assign(socket, :room, room)}
+  end
 
   def render(assigns) do
     ~H"""
-    <div>Chat Room </div>
+    <div class="flex flex-col grow shadow-lg">
+      <div class="flex justify-between items-center shrink-0 h-16 bg-white border-b border-slate-300 px-4">
+        <div class="flex flex-col gap-1.5">
+          <h1 class="text-sm font-bold leading-none">
+            #{@room.name}
+          </h1>
+
+          <div class="text-xs leading-none h-3.5">
+            {@room.topic}
+          </div>
+        </div>
+      </div>
+    </div>
     """
   end
 end
